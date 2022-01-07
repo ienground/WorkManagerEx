f3afb803e6039b99d2ae918954842aa6ca709399
IENGROUND <ericano.rhee@gmail.com>
2022-01-07 23:54:20 +0900

    Initial commit

---
+++ app/src/main/java/net/ienlab/workmanagerex/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.ienlab.workmanagerex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
